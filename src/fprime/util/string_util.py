27f8d4101bfee9bea94672bfc3dd2be0a2a678a0
janamian <saba.janamian@jpl.nasa.gov>
2021-08-19 22:22:34 -0700

    saba-ja: fix format error

--- src/fprime/util/string_util.py
+++ src/fprime/util/string_util.py
@@ -2,7 +2,7 @@
 string_util.py
 Utility functions to process strings to be used in FPrime GDS
 @Created March 18, 2021
-@janamian
+@`janamian`
 
 Note: This function has an identical copy in fprime-gds
 """
@@ -19,25 +19,25 @@ def format_string_template(format_str, given_values):
     without using python interpolation
     Considered the following format for C-string:
     %[flags][width][.precision][length]type
-    
+
     0- %:                (?<!%)(?:%%)*%
     1- flags:            ([\-\+0\ \#])?
     2- width:            (\d+|\*)?
     3- .precision:       (\.\*|\.\d+)?
     4- length:          `([hLIw]|l{1,2}|I32|I64)?`
     5- conversion_type: `([cCdiouxXeEfgGaAnpsSZ])`
-    
+
     Note:
     This function will keep the flags, width, and .precision of C-string
     template. 
-    
+
     It will keep f, d, x, o, and e flags and remove all other types.
     Other types will be duck-typed by python 
     interpreter. 
-    
+
     lengths will also be removed since they are not meaningful to Python interpreter.
     `See: https://docs.python.org/3/library/stdtypes.html#printf-style-string-formatting`
-    
+
     `Regex Source: https://www.regexlib.com/REDetails.aspx?regexp_id=3363`
     """
     def convert(match_obj, ignore_int):
@@ -57,12 +57,12 @@ def format_string_template(format_str, given_values):
                     str(conversion_type).lower() == 'x',
                     str(conversion_type).lower() == 'o',
                     str(conversion_type).lower() == 'e',
-                    ]):
+                ]):
                     format_template += f'{conversion_type}'
                 elif all([not ignore_int,
-                        str(conversion_type).lower() == 'd']):
+                          str(conversion_type).lower() == 'd']):
                     format_template += f'{conversion_type}'
-     
+
             if format_template == '':
                 template = '{}'
             else:
@@ -75,7 +75,6 @@ def format_string_template(format_str, given_values):
 
     def convert_ignore_int(match_obj):
         return convert(match_obj, ignore_int=True)
-    
 
     # Allowing single, list and tuple inputs
     if not isinstance(given_values, (list, tuple)):
@@ -88,6 +87,7 @@ def format_string_template(format_str, given_values):
     pattern = '(?<!%)(?:%%)*%([\-\+0\ \#])?(\d+|\*)?(\.\*|\.\d+)?([hLIw]|l{1,2}|I32|I64)?([cCdiouxXeEfgGaAnpsSZ])'
 
     match = re.compile(pattern)
+    
     # First try to include all types
     try:
         formated_str = re.sub(match, convert_include_all, format_str)
@@ -99,8 +99,8 @@ def format_string_template(format_str, given_values):
         msg += f'values: {values}. '
         msg += f'format_str: {format_str}. '
         LOGGER.warning(msg)
-        
-    # Second try by not including %d. 
+
+    # Second try by not including %d.
     # This will resolve failing ENUMs with %d
     # but will fail on other types.
     try:
