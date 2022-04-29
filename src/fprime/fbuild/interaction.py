2e8b8e132a4762a09cb171a463b81fd17fe33c0f
Thibault <70470823+ThibFrgsGmz@users.noreply.github.com>
2022-04-30 00:18:31 +0200

    Simplify boolean if expressions

--- src/fprime/fbuild/interaction.py
+++ src/fprime/fbuild/interaction.py
@@ -35,10 +35,7 @@ def replace_contents(filename, what, replacement, count=1):
     with open(filename, "w") as fh:
         new_file = changelog.replace(what, replacement, count)
         fh.write(new_file)
-        if new_file != changelog:
-            return True
-        else:
-            return False
+        return new_file != changelog
 
 
 def run_impl(deployment: Path, path: Path, platform: str, verbose: bool):
