6b7c949ded559284acd5fbd6dcbd18d13c0b67d5
Thibault <70470823+ThibFrgsGmz@users.noreply.github.com>
2022-06-08 21:49:41 +0200

    revise nested ternary operator

--- src/fprime/fbuild/cmake.py
+++ src/fprime/fbuild/cmake.py
@@ -105,13 +105,13 @@ class CMakeHandler:
             cmake_target = target
         else:
             module = self.get_cmake_module(path, build_dir)
-            cmake_target = (
-                module
-                if target == ""
-                else target
-                if top_target
-                else f"{module}_{target}".lstrip("_")
-            )
+            if target == "":
+                cmake_target = module
+            elif top_target:
+                cmake_target = target
+            else:
+                cmake_target = f"{module}_{target}".lstrip("_")
+            
         run_args = ["--build", build_dir]
         environment = {} if environment is None else copy.copy(environment)
         if self.verbose:
