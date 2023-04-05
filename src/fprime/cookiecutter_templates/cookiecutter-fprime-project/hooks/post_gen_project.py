9e76baecbe12e43359b83bf01aacabca3ee8f148
thomas-bc <thomas.boyerchammard@gmail.com>
2023-04-05 10:25:33 -0700

    Print instructions

--- src/fprime/cookiecutter_templates/cookiecutter-fprime-project/hooks/post_gen_project.py
+++ src/fprime/cookiecutter_templates/cookiecutter-fprime-project/hooks/post_gen_project.py
@@ -1,6 +1,30 @@
 import os
 
-print(os.system("pwd"))
 # os.chdir("{{cookiecutter.project_name}}")
+
+# Adding F' as a submodule
 os.system("git init")
 os.system("git submodule add -b {{cookiecutter.fprime_branch}} https://github.com/nasa/fprime.git")
+
+
+print("""
+
+###########################################################
+Congrats!!! You have successfully created a new F' project!
+
+Next recommended steps:
+
+-- OPTIONAL: Select your F' version --
+cd fprime
+git checkout tags/vX.X.X
+
+
+-- Generate a new component --
+fprime-util new --component
+
+-- Generate a new deployment --
+fprime-util new --deployment
+fprime-util generate
+fprime-util build
+
+""")
