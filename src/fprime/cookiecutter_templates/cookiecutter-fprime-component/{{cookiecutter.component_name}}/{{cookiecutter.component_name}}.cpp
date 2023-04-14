707badf4d5e9a8d6fba355e042c7bab8dffea721
thomas-bc <thomas.boyerchammard@gmail.com>
2023-04-14 10:52:34 -0700

    mark placeholders

--- src/fprime/cookiecutter_templates/cookiecutter-fprime-component/{{cookiecutter.component_name}}/{{cookiecutter.component_name}}.cpp
+++ src/fprime/cookiecutter_templates/cookiecutter-fprime-component/{{cookiecutter.component_name}}/{{cookiecutter.component_name}}.cpp
@@ -1,4 +1,4 @@
 // ======================================================================
 // \title  {{cookiecutter.component_name}}.cpp
-// \brief  {{cookiecutter.component_short_description}}
+// \brief  {{cookiecutter.component_short_description}} placeholder. Use fprime-util impl
 // ======================================================================
