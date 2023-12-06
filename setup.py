cf225cb76fb777a75d7b067cd8153932664316b9
thomas-bc <thomas.boyerchammard@gmail.com>
2023-12-06 13:42:35 -0800

    Review recommendations

---
+++ setup.py
@@ -0,0 +1,6 @@
+# setup.py is kept to support legacy builds
+# https://setuptools.pypa.io/en/latest/userguide/pyproject_config.html
+from setuptools import setup
+
+# Configuration is in pyproject.toml
+setup()
