d0623602c5a853c7754dcc6f7586dda41e24db28
Piyush Raj <piyush.189303055@muj.manipal.edu>
2024-01-10 08:01:09 +0530

    Add comamnds unit test skeleton (nasa/fprime#2407)

---
+++ test/fprime/util/commands_unit_test.py
@@ -0,0 +1,53 @@
+"""
+(test) fprime-utils commands:
+
+Tests the F prime util commands.
+@author 0x48piraj
+"""
+
+import unittest
+from fprime.util.commands import run_code_format, run_hash_to_file, run_info, run_new, run_sysinfo
+
+
+class CommandsTestCases(unittest.TestCase):
+    def test_build_command(self):
+        pass
+
+    def test_check_command(self):
+        pass
+
+    def test_generate_command(self):
+        pass
+
+    def test_purge_command(self):
+        pass
+
+    def test_fppcheck_command(self):
+        pass
+
+    def test_fpptoxml_command(self):
+        pass
+
+    def test_visualize_command(self):
+        pass
+
+    def test_impl_command(self):
+        pass
+
+    def test_hashtofile_command(self):
+        pass
+
+    def test_info_command(self):
+        pass
+
+    def test_new_command(self):
+        pass
+
+    def test_format_command(self):
+        pass
+
+    def test_sysinfo_command(self):
+        pass
+
+if __name__ == "__main__":
+    unittest.main()
