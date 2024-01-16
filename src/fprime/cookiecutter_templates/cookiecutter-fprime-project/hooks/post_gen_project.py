7b214b9c2547eb8ac3d29de5ffa09260fec3b524
thomas-bc <thomas.boyerchammard@gmail.com>
2024-01-16 13:08:40 -0800

    Reorder

--- src/fprime/cookiecutter_templates/cookiecutter-fprime-project/hooks/post_gen_project.py
+++ src/fprime/cookiecutter_templates/cookiecutter-fprime-project/hooks/post_gen_project.py
@@ -32,6 +32,14 @@ subprocess.run(
         "https://github.com/nasa/fprime.git",
     ]
 )
+# Checkout F´ submodules (e.g. googletest)
+res = subprocess.run(
+    ["git", "submodule", "update", "--init", "--recursive"],
+    capture_output=True,
+)
+if res.returncode != 0:
+    print("[WARNING] Unable to initialize submodules. Functionality may be limited.")
+
 subprocess.run(
     ["git", "fetch", "origin", "--depth", "1", "tag", latest_tag_name],
     cwd="./fprime",
@@ -48,14 +56,6 @@ if res.returncode != 0:
     print(f"[ERROR] Unable to checkout tag: {latest_tag_name}. Exit...")
     sys.exit(1)  # sys.exit(1) indicates failure to cookiecutter
 
-# Checkout F´ submodules (e.g. googletest)
-res = subprocess.run(
-    ["git", "submodule", "update", "--init", "--recursive"],
-    cwd="./fprime",
-    capture_output=True,
-)
-if res.returncode != 0:
-    print("[WARNING] Unable to initialize submodules. Functionality may be limited.")
 
 # Install venv if requested
 if "{{cookiecutter.__install_venv}}" == "yes":
