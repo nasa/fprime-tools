1c0a439bbc250a0cffff1a069bfdc935b8619b64
Ali Mosallaei <ali.mosallaei@jpl.nasa.gov>
2024-05-30 14:58:16 -0700

    Minor fix in error text

--- src/fprime/cookiecutter_templates/cookiecutter-fprime-subtopology/hooks/pre_gen_project.py
+++ src/fprime/cookiecutter_templates/cookiecutter-fprime-subtopology/hooks/pre_gen_project.py
@@ -4,5 +4,5 @@ name = "{{ cookiecutter.subtopology_name }}"
 
 if is_valid_name(name) != "valid":
     raise ValueError(
-        f"Unacceptable project name: {name}. Do not use spaces or special characters"
+        f"Unacceptable subtopology name: {name}. Do not use spaces or special characters"
     )
