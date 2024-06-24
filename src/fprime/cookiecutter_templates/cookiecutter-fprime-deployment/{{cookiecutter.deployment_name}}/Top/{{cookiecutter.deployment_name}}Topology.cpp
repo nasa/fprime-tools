c6f4d04500a5fa10bb41e2920f8daf01c19ab8d9
Ali Mosallaei <amosallaei19@gmail.com>
2024-06-24 12:09:12 -0700

    Fix comment in deployment Topology.cpp (#205)

--- src/fprime/cookiecutter_templates/cookiecutter-fprime-deployment/{{cookiecutter.deployment_name}}/Top/{{cookiecutter.deployment_name}}Topology.cpp
+++ src/fprime/cookiecutter_templates/cookiecutter-fprime-deployment/{{cookiecutter.deployment_name}}/Top/{{cookiecutter.deployment_name}}Topology.cpp
@@ -145,7 +145,7 @@ void setupTopology(const TopologyState& state) {
     connectComponents();
     // Autocoded command registration. Function provided by autocoder.
     regCommands();
-    // Project-specific component configuration. Function provided above. May be inlined, if desired.
+    // Deployment-specific component configuration. Function provided above. May be inlined, if desired.
     configureTopology();
     // Component-specific configurations using fpp phases. Function provided by autocoder.
     configComponents(state);
