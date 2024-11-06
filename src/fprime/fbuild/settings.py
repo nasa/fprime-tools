072f3b607fda05f564bc3a0e3ddad940c03ae54e
M Starch <Michael.D.Starch@jpl.nasa.gov>
2024-11-06 13:47:12 -0800

    Enabling extended interpolation for settings.ini

--- src/fprime/fbuild/settings.py
+++ src/fprime/fbuild/settings.py
@@ -235,7 +235,7 @@ class IniSettings:
         :param env_file: load environment from this file
         :return: environment dictionary
         """
-        parser = configparser.ConfigParser()
+        parser = configparser.ConfigParser(interpolation=configparser.ExtendedInterpolation())
         parser.optionxform = str
         parser.read(env_file)
         env_dict = {}
