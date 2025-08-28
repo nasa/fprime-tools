c0eda51844a0402e8b2d36b2aa3e4a90b23100dc
M Starch <LeStarch@googlemail.com>
2025-08-27 18:40:14 -0700

    Fix for PR-4087

--- src/fprime/cookiecutter_templates/cookiecutter-fprime-deployment/{{cookiecutter.deployment_name}}/Top/{{cookiecutter.deployment_name}}Topology.cpp
+++ src/fprime/cookiecutter_templates/cookiecutter-fprime-deployment/{{cookiecutter.deployment_name}}/Top/{{cookiecutter.deployment_name}}Topology.cpp
@@ -95,12 +95,12 @@ void setupTopology(const TopologyState& state) {
 {%- endif %}
 }
 
-void startRateGroups(Fw::TimeInterval interval) {
+void startRateGroups(const Fw::TimeInterval& interval) {
     // The timer component drives the fundamental tick rate of the system.
     // Svc::RateGroupDriver will divide this down to the slower rate groups.
     // This call will block until the stopRateGroups() call is made.
     // For this Linux demo, that call is made from a signal handler.
-    timer.startTimer(interval.getSeconds()*1000+interval.getUSeconds()/1000);
+    timer.startTimer(interval);
 }
 
 void stopRateGroups() {
