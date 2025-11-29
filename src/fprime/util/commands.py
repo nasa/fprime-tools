40c6b724eee4e001393d3ea878b90bd80bc7e488
rmzmrnn <marananremzell@gmail.com>
2025-11-29 18:04:05 +0800

    Apply black format

--- src/fprime/util/commands.py
+++ src/fprime/util/commands.py
@@ -205,7 +205,9 @@ def run_code_format(
     for excluded_dir in parsed.exclude:
         excluded_path = Path(excluded_dir)
         if not excluded_path.is_dir():
-            print(f"[INFO] {excluded_path} is not a directory. Skipping it from excluded directories.")
+            print(
+                f"[INFO] {excluded_path} is not a directory. Skipping it from excluded directories."
+            )
             continue
         for allowed_ext in clang_formatter.allowed_extensions:
             for file in excluded_path.rglob(f"*{allowed_ext}"):
